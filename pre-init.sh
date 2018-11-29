19d5708b5eb0018876952b0964434cb9e9d0d372
Subrata Maji <Subrata.Maji@ingrammicro.com>
2018-11-29 11:33:32 +0530

    jenkins scripts added

---
+++ pre-init.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "System Preparation"
+
+echo "Installing Ansible...."
+echo Install pre-requisite packages
+sudo yum check-update; sudo yum install -y gcc libffi-devel python-devel openssl-devel epel-release
+sudo yum install -y python-pip python-wheel
+
+echo "Install Ansible and Azure SDKs via pip"
+sudo pip install ansible[azure]
+
+sudo yum install -y git
+
+echo "Installing docker"
+sudo yum install -y docker
+sudo systemctl enable docker && systemctl start docker
+
+sudo yum install -y java
+
+sudo docker build -f Dockerfile -t cmpjenkins:latest Jenkins/.
+sudo docker run -itd -p 8282:8080 -p 50001:50000 -v jenkins-data:/var/jenkins_home --name=jenkins-cmp cmpjenkins
